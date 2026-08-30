f536f13d34f6b319087be0b3465e5dc65702e297
polina <67948937+PolinaRG@users.noreply.github.com>
2026-08-30 19:45:34 +0200

    feat: add steady state solution

--- lotka_volterra_model_study.m
+++ lotka_volterra_model_study.m
@@ -3,15 +3,21 @@ betta = 0.5;    % betta is predator death rate: d(N_predator) / dt ~ -betta * N_
 c = 0.5;        % c shows how predator population affects the growth of prey population (negative effect)
 d = 0.01;      % d shows how prey population affects the growth of predator population (positive effect)
 
-prey_number_stable = betta / d;
-predator_number_stable = alpha / c;
+steady_state_prey = betta / d;
+steady_state_predator = alpha / c;
 
 tspan = [0, 30];
 y0 = [0.8 * prey_number_stable, 1.2 * predator_number_stable];
 [t, y] = ode45(@(t, y) lotka_volterra_ode(t, y, alpha, betta, c, d), tspan, y0);
 
-plot(t, y(:, 1), '-.', t, y(:, 2), '-.')
+figure;
+plot(t, y(:, 1), 'Color', '#2BB735', 'LineWidth', 2, t, y(:, 2), 'Color', '#FF0000', 'LineWidth', 2)
 xlabel('time, years')
 ylabel('population')
-legend('prey (rabbits)', 'predator (foxes)')
 grid on
+hold on
+
+y0_steady_state = [steady_state_prey, steady_state_predator];
+[t_steady_sate, y_steady_state] = ode45(@(t, y) lotka_volterra_ode(t, y, alpha, betta, c, d), tspan, y0_steady_state);
+plot(t_steady_sate, y_steady_state(:, 1), 'Color', '#FEAB12', t_steady_sate, y_steady_state(:, 2), 'Color', '#0135E7')
+legend('prey (rabbits)', 'predator (foxes)', 'steady state: prey', 'steady state: predator')
