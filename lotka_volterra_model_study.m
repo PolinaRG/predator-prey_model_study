288965fc88a316e70d4bfe96a6618741a0321519
polina <67948937+PolinaRG@users.noreply.github.com>
2026-08-31 23:24:54 +0200

    feat: get and compare characteristics of the curves

--- lotka_volterra_model_study.m
+++ lotka_volterra_model_study.m
@@ -6,7 +6,8 @@ d = 0.01;      % d shows how prey population affects the growth of predator popu
 steady_state_prey = betta / d;
 steady_state_predator = alpha / c;
 
-tspan = [0, 30];
+tspan_years = 30;
+tspan = [0, tspan_years];
 y0 = [0.8 * steady_state_prey, 1.2 * steady_state_predator];
 [t, y] = ode45(@(t, y) lotka_volterra_ode(t, y, alpha, betta, c, d), tspan, y0);
 
@@ -23,3 +24,14 @@ y0_steady_state = [steady_state_prey, steady_state_predator];
 [t_steady_sate, y_steady_state] = ode45(@(t, y) lotka_volterra_ode(t, y, alpha, betta, c, d), tspan, y0_steady_state);
 plot(t_steady_sate, y_steady_state(:, 1), 'Color', '#FEAB12', t_steady_sate, y_steady_state(:, 2), 'Color', '#FF0000')
 legend('prey (rabbits)', 'predator (foxes)', 'steady state: prey', 'steady state: predator')
+
+[peaks_preys, peak_indices_preys] = findpeaks(y(:, 1));
+max_years_preys = round(t(peak_indices_preys));
+
+extra_positive_addition = max(y(:, 2)) + 1;
+[~, bottoms_indices_predators] = findpeaks(extra_positive_addition - y(:, 2));
+bottoms_predators = y(bottoms_indices_predators, 2);
+min_years_predators = round(t(bottoms_indices_predators));
+
+mean_period_preys = mean(diff(max_years_preys));
+mean_period_predators = mean(diff(min_years_predators));
