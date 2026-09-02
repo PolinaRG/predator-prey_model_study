675812c9638d3543e9af464a037566ca8f71703f
polina <67948937+PolinaRG@users.noreply.github.com>
2026-09-02 12:11:21 +0200

    refactor: move parameters control to the main script

--- main_predator_prey_model_study.m
+++ main_predator_prey_model_study.m
@@ -1,1 +1,6 @@
-study_lotka_volterra_model()
+alpha = 5;
+betta = 0.5;
+c = 0.5;
+d = 0.01;
+
+study_lotka_volterra_model(alpha, betta, c, d, 30, [0.8, 1.2])
--- study_lotka_volterra_model.m
+++ study_lotka_volterra_model.m
@@ -1,15 +1,13 @@
-function study_lotka_volterra_model()
-  alpha = 5;      % alpha is prey birth rate: d(N_prey)/dt ~ alpha * N_prey
-  betta = 0.5;    % betta is predator death rate: d(N_predator) / dt ~ -betta * N_predator
-  c = 0.5;        % c shows how predator population affects the growth of prey population (negative effect)
-  d = 0.01;      % d shows how prey population affects the growth of predator population (positive effect)
-
+% alpha is prey birth rate: d(N_prey)/dt ~ alpha * N_prey
+% betta is predator death rate: d(N_predator) / dt ~ -betta * N_predator
+% c shows how predator population affects the growth of prey population (negative effect)
+% d shows how prey population affects the growth of predator population (positive effect)
+function study_lotka_volterra_model(alpha, betta, c, d, tspan_years, y0_part_of_steady_state)
   steady_state_prey = betta / d;
   steady_state_predator = alpha / c;
 
-  tspan_years = 30;
   tspan = 0:0.05:tspan_years;
-  y0 = [0.8 * steady_state_prey, 1.2 * steady_state_predator];
+  y0 = [steady_state_prey, steady_state_predator] .* y0_part_of_steady_state;
   [t, y] = ode45(@(t, y) lotka_volterra_ode(t, y, alpha, betta, c, d), tspan, y0);
 
   figure;
