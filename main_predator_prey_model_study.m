04767b8cf5ac9e078a3bb7c1f3be70a3153454c7
polina <67948937+PolinaRG@users.noreply.github.com>
2026-09-02 12:47:28 +0200

    feat: add model parameters from numerical experiment published on wikipedia

--- main_predator_prey_model_study.m
+++ main_predator_prey_model_study.m
@@ -5,3 +5,11 @@ d = 0.01;
 y0 = [betta / d * 0.8, alpha / c * 1.2];
 
 study_lotka_volterra_model(alpha, betta, c, d, 30, y0)
+
+% numerical experiment with parameters proposed on https://en.wikipedia.org/wiki/Lotka-Volterra_equations
+##alpha = 1.1;
+##betta = 0.4;
+##c = 0.4;
+##d = 0.1;
+##
+##study_lotka_volterra_model(alpha, betta, c, d, 100, [10, 10])
