95ee2cfd006ceb689c682b351636c6ad260ac98b
polina <67948937+PolinaRG@users.noreply.github.com>
2026-09-02 21:05:10 +0200

    refactor: unify parameters usage

--- main_predator_prey_model_study.m
+++ main_predator_prey_model_study.m
@@ -2,10 +2,15 @@ alpha = 5;
 betta = 0.5;
 c = 0.5;
 d = 0.01;
-y0 = [betta / d * 0.8, alpha / c * 1.2];
 
-study_lotka_volterra_model(alpha, betta, c, d, 30, y0)
-plot_phase_space_for_lv_model(alpha, betta, c, d, round([betta / d, alpha / c] * 0.1), 30, 5)
+n_years = 30;
+
+steady_state = [betta / d, alpha / c];
+
+y0 = steady_state .* [0.8, 1.2];
+
+study_lotka_volterra_model(alpha, betta, c, d, n_years, y0)
+plot_phase_space_for_lv_model(alpha, betta, c, d, round(steady_state * 0.1), n_years, 5)
 
 % numerical experiment with parameters proposed on https://en.wikipedia.org/wiki/Lotka-Volterra_equations
 ##alpha = 1.1;
