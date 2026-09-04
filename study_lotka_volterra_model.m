50ddafe4389eb67c349f54e9dffbc202d0a412e8
polina <67948937+PolinaRG@users.noreply.github.com>
2026-09-04 20:55:59 +0200

    feat: plot population densities in two subplots

--- study_lotka_volterra_model.m
+++ study_lotka_volterra_model.m
@@ -13,6 +13,7 @@ function study_lotka_volterra_model(alpha, betta, c, d, tspan_years, y0)
   [t_steady_sate, y_steady_state] = ode45(@(t, y) lotka_volterra_ode(t, y, alpha, betta, c, d), tspan, y0_steady_state);
 
   plot_in_common_axes(t, y, t_steady_sate, y_steady_state)
+  plot_in_subplots(t, y, t_steady_sate, y_steady_state)
 
   [peaks_preys, peak_indices_preys] = findpeaks(y(:, 1));
   max_years_preys = round(t(peak_indices_preys));
@@ -37,4 +38,26 @@ function plot_in_common_axes(t, y, t_steady_sate, y_steady_state)
 
   plot(t_steady_sate, y_steady_state(:, 1), 'Color', '#FEAB12', t_steady_sate, y_steady_state(:, 2), 'Color', '#FF0000')
   legend('prey (rabbits)', 'predator (foxes)', 'steady state: prey', 'steady state: predator')
+end
+
+function plot_in_subplots(t, y, t_steady_sate, y_steady_state)
+  figure;
+
+  subplot(2, 1, 1);
+  plot(t, y(:, 1), 'Color', '#2BB735');
+  title('Prey population over time');
+  xlabel('time, years');
+  ylabel('prey population');
+  grid on
+  hold on
+  plot(t_steady_sate, y_steady_state(:, 1), 'Color', '#FEAB12')
+
+  subplot(2, 1, 2);
+  plot(t, y(:, 2), 'Color', '#0135E7');
+  title('Predator population over time');
+  xlabel('time, years');
+  ylabel('predator population');
+  grid on
+  hold on
+  plot(t_steady_sate, y_steady_state(:, 2), 'Color', '#FF0000')
 end
