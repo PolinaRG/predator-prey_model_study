dc206001a87f69ff13d94803c100e0226bd9cb06
polina <67948937+PolinaRG@users.noreply.github.com>
2026-09-04 17:28:21 +0200

    feat: change parameter d and step for phase-space plot

--- main_predator_prey_model_study.m
+++ main_predator_prey_model_study.m
@@ -1,7 +1,7 @@
 alpha = 5;
 betta = 0.5;
 c = 0.5;
-d = 0.01;
+d = 0.005;
 
 n_years = 30;
 
@@ -10,7 +10,7 @@ steady_state = [betta / d, alpha / c];
 y0 = steady_state .* [0.8, 1.2];
 
 study_lotka_volterra_model(alpha, betta, c, d, n_years, y0)
-plot_phase_space_for_lv_model(alpha, betta, c, d, round(steady_state * 0.1), n_years, 5)
+plot_phase_space_for_lv_model(alpha, betta, c, d, [10, 5], n_years, 5)
 
 % numerical experiment with parameters proposed on https://en.wikipedia.org/wiki/Lotka-Volterra_equations
 ##alpha = 1.1;
