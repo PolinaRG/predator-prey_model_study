319ceac5183acf0b10e687a115585d886244bc80
polina <67948937+PolinaRG@users.noreply.github.com>
2026-09-05 10:41:59 +0200

    refactor: remove unused code left from numerical experiment with parameters from wikipedia

--- main_predator_prey_model_study.m
+++ main_predator_prey_model_study.m
@@ -13,12 +13,3 @@ palette = containers.Map({'prey', 'predator', 'prey_steady_state', 'predator_ste
 
 study_lotka_volterra_model(model, n_years, y0, palette)
 plot_phase_space_for_lv_model(model, [10, 5], n_years, 5)
-
-% numerical experiment with parameters proposed on https://en.wikipedia.org/wiki/Lotka-Volterra_equations
-##alpha = 1.1;
-##betta = 0.4;
-##c = 0.4;
-##d = 0.1;
-##
-##study_lotka_volterra_model(alpha, betta, c, d, 100, [10, 10])
-##plot_phase_space_for_lv_model(alpha, betta, c, d, [2, 2], 100, 5)
